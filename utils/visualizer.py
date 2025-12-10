799deaec395b0a83b32e0ed27fa6f2c8ee90557d
yoyoyo0807 <rerere182152815211@gmail.com>
2025-12-10 10:37:41 +0900

    Rename parameter in plot_mesh_risk_map function

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -1,21 +1,22 @@
 # utils/visualizer.py
 import plotly.express as px
 
-def plot_mesh_risk_map(df, color_col="risk_score", title="リスクマップ"):
+def plot_mesh_risk_map(df_mesh, color_col="risk_score", title="都市リスクマップ"):
     fig = px.scatter_mapbox(
-        df,
+        df_mesh,
         lat="lat",
         lon="lon",
         color=color_col,
+        color_continuous_scale="RdYlGn_r",
         size="n_cases",
         hover_name="mesh_id",
         hover_data=["risk_score", "n_cases"],
         zoom=11,
-        height=600,
+        height=600
     )
     fig.update_layout(
         mapbox_style="carto-positron",
-        title=title,
         margin=dict(l=0, r=0, t=40, b=0),
+        title=title
     )
     return fig
